f343f57266abb9949ff275025f4ab1094a97aa33
Danylo <danya.pac3@gmail.com>
2026-02-19 22:15:43 +0200

    Make personal form save data from inputs to state

--- src/components/input-group.jsx
+++ src/components/input-group.jsx
@@ -1,10 +1,22 @@
 import "../styles/input-group.css";
 
-function InputGroup({ name, onChange = () => {}, label, type = "text" }) {
+function InputGroup({
+  name,
+  value,
+  handleChange = () => {},
+  label,
+  type = "text",
+}) {
   return (
     <div className="input-group">
       <label htmlFor={name}>{label} :</label>
-      <input onChange={onChange} type={type} id={name} name={name}></input>
+      <input
+        onChange={(e) => handleChange(name, e.target.value)}
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+      />
     </div>
   );
 }
--- src/components/personal-info-form.jsx
+++ src/components/personal-info-form.jsx
@@ -1,19 +1,80 @@
 import "../styles/personal-info-form.css";
 
 import InputGroup from "./input-group";
+import { useState } from "react";
+
+const initialData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  jobTarget: "",
+  dateOfBirth: "",
+  country: "",
+  city: "",
+};
 
 export default function PersonalInfoForm() {
+  const [formData, setData] = useState(initialData);
+
+  const handleChange = (name, value) => {
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   return (
-    <form className="personal-info-form cv-form">
+    <form>
       <h1 className="cv-form__title">Personal Information</h1>
-      <InputGroup name="firstName" label="First Name" />
-      <InputGroup name="lastName" label="Last Name" />
-      <InputGroup name="email" type="email" label="Email" />
-      <InputGroup name="phoneNumber" type="tel" label="Phone Number" />
-      <InputGroup name="jobTarget" label="Job Target" />
-      <InputGroup name="dateOfBirth" type="date" label="Date Of Birth" />
-      <InputGroup name="country" label="Country" />
-      <InputGroup name="city" label="City" />
+      <InputGroup
+        name="firstName"
+        label="First Name"
+        value={formData.firstName}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="lastName"
+        label="Last Name"
+        value={formData.lastName}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="email"
+        type="email"
+        label="Email"
+        value={formData.email}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="phoneNumber"
+        type="tel"
+        label="Phone Number"
+        value={formData.phoneNumber}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="jobTarget"
+        label="Job Target"
+        value={formData.jobTarget}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="dateOfBirth"
+        type="date"
+        label="Date Of Birth"
+        value={formData.dateOfBirth}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="country"
+        label="Country"
+        value={formData.country}
+        handleChange={handleChange}
+      />
+      <InputGroup
+        name="city"
+        label="City"
+        value={formData.city}
+        handleChange={handleChange}
+      />
     </form>
   );
 }
