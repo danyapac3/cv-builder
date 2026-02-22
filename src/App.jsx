7d004b5db03c4774257ef39fde61e25304443960
Danylo <danya.pac3@gmail.com>
2026-02-22 16:07:30 +0200

    add styling for next and prev buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./styles/App.css";
+import "./styles/app.css";
 
 import PersonalInfoForm from "./components/personal-info-form";
 import EmploymentHistoryForm from "./components/employment-history-form";
@@ -20,22 +20,26 @@ function App() {
   const CurrentForm = formComponents[formIndex];
 
   return (
-    <div className="form-container">
-      <CurrentForm />
-      <button
-        disabled={formIndex <= 0}
-        className="prev-form-button"
-        onClick={handlePrevForm}
-      >
-        Back
-      </button>
-      <button
-        disabled={formIndex >= formComponents.length - 1}
-        className="next-form-button"
-        onClick={handleNextForm}
-      >
-        Next
-      </button>
+    <div className="app">
+      <div className="app__form-container">
+        <CurrentForm />
+        <div className="app__form-controls">
+          <button
+            className="app__prev-form-button"
+            disabled={formIndex <= 0}
+            onClick={handlePrevForm}
+          >
+            Back
+          </button>
+          <button
+            disabled={formIndex >= formComponents.length - 1}
+            className="app__next-form-button"
+            onClick={handleNextForm}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
